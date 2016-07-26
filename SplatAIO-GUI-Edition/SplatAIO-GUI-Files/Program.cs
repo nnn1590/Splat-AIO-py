0cdee34e10b6021ae5f559ccf05897b39e6269a5
™DJ Woomy ᕦ( ͡° ͜ʖ ͡°)ᕤ <mcminers9@gmail.com>
2016-07-25 21:02:42 -0400

    Add files via upload

---
+++ SplatAIO-GUI-Edition/SplatAIO-GUI-Files/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SplatAIO
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // for debugging purposes
+            // CultureInfo japaneseCulture = new CultureInfo("ja-JP");
+            // Thread.CurrentThread.CurrentCulture = japaneseCulture;
+            // Thread.CurrentThread.CurrentUICulture = japaneseCulture;
+            // CultureInfo.DefaultThreadCurrentCulture = japaneseCulture;
+            // CultureInfo.DefaultThreadCurrentUICulture = japaneseCulture;
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
